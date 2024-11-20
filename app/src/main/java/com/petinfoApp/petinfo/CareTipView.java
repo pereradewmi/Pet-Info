ce90cabdf1547d45f4c3e7ac25049e40a153f4ce
Dewmi Perera <101668574+pereradewmi@users.noreply.github.com>
2024-11-20 03:39:51 -0800

    done

--- app/src/main/java/com/petinfoApp/petinfo/CareTipView.java
+++ app/src/main/java/com/petinfoApp/petinfo/CareTipView.java
@@ -1,11 +1,6 @@
 package com.petinfoApp.petinfo;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class NewCareTips extends AppCompatActivity {
+public class CareTipView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_new_care_tips);
+        setContentView(R.layout.activity_care_tip_view);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        LinearLayout layout = findViewById(R.id.careTip1);
-        layout.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(NewCareTips.this, SingleCareTip.class);
-                startActivity(intent);
-            }
         });
     }
 }
